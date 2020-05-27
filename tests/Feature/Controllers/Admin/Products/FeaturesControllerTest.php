a57bea7f11522563971c3c9fced992d839b1d6bb
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2020-05-27 14:23:58 +0200

    Tests: Product Features Resource

---
+++ tests/Feature/Controllers/Admin/Products/FeaturesControllerTest.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Tests\Feature\Controllers\Admin\Products;
+
+use Tests\TestCase;
+use Illuminate\Support\Str;
+use App\Models\ProductFeature;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FeaturesControllerTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    protected $table = 'product_features';
+
+    protected $resourceName = 'Product Feature';
+
+    protected $path = '/admin/products/features';
+
+    protected $viewPath = 'admin.products.features';
+
+    protected $model = ProductFeature::class;
+
+    /** @test */
+    public function guests_cannot_access_resource_actions()
+    {
+        $resource = factory($this->model)->create();
+
+        // list
+        $this->get($this->path)->assertRedirect($this->loginPath);
+        // create
+        $this->get("{$this->path}/create")->assertRedirect($this->loginPath);
+        // store
+        $this->post($this->path, $resource->toArray())->assertRedirect($this->loginPath);
+        // show
+        $this->get("{$this->path}/{$resource->id}")->assertRedirect($this->loginPath);
+        // edit
+        $this->get("{$this->path}/{$resource->id}/edit")->assertRedirect($this->loginPath);
+        // update
+        $this->put("{$this->path}/{$resource->id}", $resource->toArray())
+            ->assertRedirect($this->loginPath);
+        // delete
+        $this->delete("{$this->path}/{$resource->id}")->assertRedirect($this->loginPath);
+    }
+
+    /** @test */
+    public function user_can_view()
+    {
+        $this->withoutExceptionHandling();
+        $this->signInAdmin();
+
+        $this->get($this->path)
+            ->assertStatus(200)
+            ->assertSee(Str::plural($this->resourceName))
+            ->assertViewIs("{$this->viewPath}.index");
+    }
+
+    /** @test */
+    public function user_can_create()
+    {
+        //$this->withoutExceptionHandling();
+        $this->signInAdmin();
+
+        // view create form
+        $this->get("{$this->path}/create")
+            ->assertStatus(200)
+            ->assertSee($this->resourceName)
+            ->assertViewIs("{$this->viewPath}.create_edit");
+
+        // view home (to save the redirect url in session)
+        $this->get($this->path);
+
+        // create and save resource
+        $attributes = factory($this->model)->make()->toArray();
+
+        // submit form
+        $this->followingRedirects()
+            ->from("{$this->path}/create")
+            ->post($this->path, $attributes)
+            ->assertViewIs("{$this->viewPath}.index")
+            ->assertSee($attributes['name'])
+            ->assertSee(Str::plural($this->resourceName));
+
+        $this->assertDatabaseHas($this->table, ['name' => $attributes['name']]);
+    }
+
+    /** @test */
+    public function validate_on_create()
+    {
+        $this->signInAdmin();
+
+        $this->get($this->path);
+
+        $this->get("{$this->path}/create")->assertStatus(200)->assertSee($this->resourceName);
+
+        $attributes = factory($this->model)->raw([
+            'name'    => null,
+        ]);
+
+        $this->post($this->path, $attributes)
+            ->assertSessionHasErrors(['name']);
+    }
+
+    /** @test */
+    public function user_can_show()
+    {
+        $this->signInAdmin();
+
+        $resource = factory($this->model)->create();
+
+        $this->get("{$this->path}/{$resource->id}")
+            ->assertSee($resource->name)
+            ->assertSee($this->resourceName);
+    }
+
+    /** @test */
+    public function user_can_update()
+    {
+        $this->signInAdmin();
+
+        $resource = factory($this->model)->create();
+
+        $this->get("{$this->path}/{$resource->id}/edit")->assertStatus(200);
+
+        $resource->name = 'new-name';
+
+        $this->get($this->path);
+
+        $this->followingRedirects()
+            ->put("{$this->path}/{$resource->id}", $resource->toArray())
+            ->assertViewIs("{$this->viewPath}.index")
+            ->assertSee($resource->name)
+            ->assertSee(Str::plural($this->resourceName));
+
+        $this->assertDatabaseHas($this->table, [
+            'id'       => $resource->id,
+            'name' => 'new-name'
+        ]);
+    }
+
+    /** @test */
+    public function validate_on_update()
+    {
+        $this->signInAdmin();
+
+        $resource = factory($this->model)->create();
+
+        $this->get("{$this->path}/{$resource->id}/edit")->assertStatus(200);
+
+        $resource->name = null;
+
+        $this->put("{$this->path}/{$resource->id}", $resource->toArray())
+            ->assertSessionHasErrors(['name']);
+    }
+
+    /** @test */
+    public function destroy()
+    {
+        $this->signInAdmin();
+
+        $resource = factory($this->model)->create();
+
+        $this->delete("{$this->path}/{$resource->id}", ['_id' => $resource->id]);
+
+        $this->assertSoftDeleted($this->table, ['id' => $resource->id]);
+    }
+}
