b878b0cd9e61c0e422b9701f2b357ce6507f06d9
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2020-09-30 11:29:26 +0200

    Photo Factory

---
+++ database/factories/PhotoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Photo;
+use Faker\Generator as Faker;
+
+$factory->define(Photo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/admin.php
+++ routes/admin.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Route;
 |------------------------------------------
 */
 Route::group(['middleware' => ['auth', 'auth.admin'], 'prefix' => 'admin', 'namespace' => 'Admin'], function () {
-
     Route::get('/', 'DashboardController@index');
 
     // profile
