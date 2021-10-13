4ae7b51321a306b981e9c0692ef05b10572ca806
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2021-10-13 14:31:21 +0100

    Update Navigation Factory and Unit Test

--- app/Models/Navigation.php
+++ app/Models/Navigation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -10,7 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Navigation extends AdminModel
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'navigations';
 
--- database/factories/NavigationFactory.php
+++ database/factories/NavigationFactory.php
@@ -1,28 +1,44 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\Models\Navigation;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(Navigation::class, function (Faker $faker) {
-    return [
-        'icon'        => 'home',
-        'name'        => 'Click Me',
-        'slug'        => 'click-me',
-        'description' => 'Lorem Ipsum',
-        'url'         => '/click-me',
-        'help_index_title'    => '',
-        'help_index_content'  => '',
-        'help_create_title'   => '',
-        'help_create_content' => '',
-        'help_edit_title'     => '',
-        'help_edit_content'   => '',
-        'list_order'          => 1,
-        'is_hidden'           => 0,
-        'parent_id'           => 0,
-        'url_parent_id'       => 0,
-        'created_by'          => 1,
-        'updated_by'          => 1,
-    ];
-});
+class NavigationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Navigation::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'icon'        => 'home',
+            'name'        => 'Click Me',
+            'slug'        => 'click-me',
+            'description' => 'Lorem Ipsum',
+            'url'         => '/click-me',
+            'help_index_title'    => '',
+            'help_index_content'  => '',
+            'help_create_title'   => '',
+            'help_create_content' => '',
+            'help_edit_title'     => '',
+            'help_edit_content'   => '',
+            'list_order'          => 1,
+            'is_hidden'           => 0,
+            'parent_id'           => 0,
+            'url_parent_id'       => 0,
+            'created_by'          => 1,
+            'updated_by'          => 1,
+        ];
+    }
+}
